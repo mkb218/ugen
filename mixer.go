deaa6a508f1bd7c32563a09534a3fea2d2eb2b40
Matt Kane <mkb-prime@hydrogenproject.com>
2012-10-03 10:28:14 -0400

    initial commit

---
+++ mixer.go
@@ -0,0 +1,70 @@
+package ugen
+
+type q struct{}
+
+type Mixer struct {
+	UGenBase
+	gains []float32
+	running bool
+	quitchan chan q
+}
+
+func init() {
+	var _ UGen = NewMixer(1)
+}
+
+func NewMixer(inchan int) *Mixer {
+	var m Mixer
+	m.outchans = append(m.outchans, make(chan float32))
+	m.gains = make([]float32, inchan)
+	m.inputs = make([]UGen, inchan)
+	m.quitchan = make(chan q)
+	return &m
+}
+
+func (m *Mixer) Stop() error {
+	m.quitchan <- q{}
+	for _, u := range m.inputs {
+		u.Stop()
+	}
+	return nil
+}
+
+func (m *Mixer) Start() error {
+	go func() {
+		for {
+			select {
+			case <- m.quitchan:
+				return
+			default:
+			}
+			
+			var o float32
+			for i, u := range m.inputs {
+				for _, c := range u.OutputChannels() {
+					select {
+					case <- m.quitchan:
+						return
+					default:
+					}
+					o += m.gains[i]* (<- c)
+				}
+			}
+			select {
+			case <- m.quitchan:
+				return
+			default:
+			}
+			m.outchans[0] <- o
+		}
+	}()
+	return nil
+}
+
+func (m *Mixer) SetGain(i int, g float32) error {
+	if i < 0 || i >= len(m.gains) {
+		return BadInputSet{i, len(m.gains)}
+	}
+	m.gains[i] = g
+	return nil
+}
---
+++ ugen.go
@@ -0,0 +1,174 @@
+package ugen
+
+import "sync"
+import "fmt"
+import "code.google.com/p/portaudio-go/portaudio"
+
+type Universe struct {
+	SampleRate, InputChannels, OutputChannels float64
+	BufferSize int
+	output *Output
+	input *Input
+}
+
+type UGen interface {
+	Inputs() []UGen
+	SetInput(UGen, int) error
+	OutputChannels() []chan float32
+	Start() error
+	Stop() error
+}
+
+type UGenBase struct {
+	inputs []UGen
+	outchans []chan float32
+}
+
+func (u *UGenBase) OutputChannels() []chan float32 {
+	return u.outchans
+}
+
+func (u *UGenBase) Inputs() []UGen {
+	return u.inputs
+}
+
+type BadInputSet struct {
+	index, length int
+}
+
+func (b BadInputSet) Error() string {
+	return fmt.Sprintf("Attempted to set bad input %d on inputs of length %d", b.index, b.length)
+}
+
+func (u *UGenBase) SetInput(g UGen, i int) error {
+	if i < 0 || i >= len(u.inputs) {
+		return BadInputSet{i, len(u.inputs)}
+	}
+	
+	u.inputs[i] = g
+	return nil
+}
+
+type Output struct {
+	UGenBase
+	universe *Universe
+	start, stop *sync.Once
+	stream *portaudio.Stream
+}
+
+func NewOutput(inchan int) *Output {
+	var o Output
+	o.inputs = make([]UGen, inchan)
+	o.resetonces()
+	return &o
+}
+
+func (o *Output) resetonces() {
+	o.start = new(sync.Once)
+	o.stop = new(sync.Once)
+}
+
+func (o *Output) Start() error {
+	var err error
+	o.start.Do(func () {
+		if o.stream == nil {
+			o.stream, err = portaudio.OpenDefaultStream(0, len(o.inputs), o.universe.SampleRate, o.universe.BufferSize, o)
+			if err != nil {
+				return
+			}
+		}
+		for _, u := range o.inputs {
+			u.Start()
+		}
+		o.stream.Start()
+	})
+	return err
+}
+
+func (o *Output) ProcessAudio(_, out [][]float32) {
+	var wg sync.WaitGroup
+	for i, b := range out {
+		wg.Add(1)
+		go func(i int, b []float32) {
+			for j := range b {
+				b[j] = <- o.inputs[i].OutputChannels()[i]
+			}
+			wg.Done()
+		}(i, b)
+	}
+	wg.Wait()
+}
+
+func (o *Output) Stop() error {
+	o.stop.Do(func () {
+		o.stream.Stop()
+		for _, u := range o.inputs {
+			u.Stop()
+		}
+	})
+	o.resetonces()
+	return nil
+}
+
+func init() {
+	var _ UGen = NewOutput(1)
+	var _ UGen = NewInput(1)
+}
+
+type Input struct {
+	UGenBase
+	universe *Universe
+	start, stop *sync.Once
+	stream *portaudio.Stream
+}
+
+
+func NewInput(outchan int) *Input {
+	var i Input
+	i.outchans = make([]chan float32, outchan, i.universe.BufferSize)
+	i.resetonces()
+	return &i
+}
+
+func (i *Input) resetonces() {
+	i.start = new(sync.Once)
+	i.stop = new(sync.Once)
+}
+
+func (i *Input) Start() error {
+	var err error
+	i.start.Do(func () {
+		if i.stream == nil {
+			i.stream, err = portaudio.OpenDefaultStream(len(i.outchans), 0, i.universe.SampleRate, i.universe.BufferSize, i)
+			if err != nil {
+				return
+			}
+		}
+		i.stream.Start()
+	})
+	return err
+}
+
+func (inp *Input) ProcessAudio(in, _ [][]float32) {
+	var wg sync.WaitGroup
+	for i, b := range in {
+		wg.Add(1)
+		go func(i int, b []float32) {
+			for j := range b {
+				inp.outchans[i] <- b[j]
+			}
+			wg.Done()
+		}(i, b)
+	}
+	wg.Wait()
+}
+
+func (i *Input) Stop() error {
+	var err error
+	i.stop.Do(func () {
+		err = i.stream.Stop()
+	})
+	i.resetonces()
+	return err
+}
+
